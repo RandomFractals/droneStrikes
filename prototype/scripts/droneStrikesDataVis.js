b6100538a3b14ca05c57db88402802f8883d3641
Taras Novak <taras.novak@randomfractals.com>
2015-12-11 18:55:47 -0600

    added api.dronestre.am/data load + some logging

--- prototype/scripts/droneStrikesDataVis.js
+++ prototype/scripts/droneStrikesDataVis.js
@@ -1,5 +1,7 @@
 $(function() {
 
+	var droneStrikes = new DroneStrikes();
+	
 	var map = L.map('map').setView([29.0, 41.0], 3); // middle east lat/long + zoom
 	//map.locate({setView: true, maxZoom: 4}); // for geo-loc based on ip later
 	
@@ -7,4 +9,26 @@ $(function() {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	
+	// get data
+	var dataUrl = 'http://api.dronestre.am/data';
+	$.get(dataUrl).done(loadData).fail(dataLoadError);
+	
+	function loadData(hitData) {
+		//console.log(hitData.strike);		
+		droneStrikes.addHits(hitData.strike);
+		
+		console.log(droneStrikes.logStats());
+		
+		$('#dataMessage').text(
+			droneStrikes.hitList.length + ' strikes since ' + 
+			droneStrikes.startTime.getMonth() + '/' +
+			droneStrikes.startTime.getDate() + '/' +
+			droneStrikes.startTime.getFullYear() );
+	}
+	
+	function dataLoadError() {
+		console.log('data load error');
+		// show data load error msg
+		$('#dataMessage').text('Failed to load drone strikes data. Check data source site.');
+	}
 });
