bf0dadc1c4eb861a83502be4b603afc7a270ead6
Taras Novak <taras.novak@randomfractals.com>
2015-12-11 21:07:53 -0600

    added hit markers to map, need to cluster them

--- prototype/scripts/droneStrikesDataVis.js
+++ prototype/scripts/droneStrikesDataVis.js
@@ -5,9 +5,10 @@ $(function() {
 	var map = L.map('map').setView([29.0, 41.0], 3); // middle east lat/long + zoom
 	//map.locate({setView: true, maxZoom: 4}); // for geo-loc based on ip later
 	
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+	// osm map
+	var mapLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+	//var mapLayer = new L.StamenTileLayer('watercolor');
+	map.addLayer(mapLayer);
 	
 	// get data
 	var dataUrl = 'http://api.dronestre.am/data';
@@ -15,10 +16,17 @@ $(function() {
 	
 	function loadData(hitData) {
 		//console.log(hitData.strike);		
-		droneStrikes.addHits(hitData.strike);
-		
+		droneStrikes.addHits(hitData.strike);		
 		console.log(droneStrikes.logStats());
 		
+		// add map markers
+		for (var i=0; i<droneStrikes.hitList.length; i++) {
+			L.marker([droneStrikes.hitList[i].latitude,
+					droneStrikes.hitList[i].longitude
+				]).addTo(map);
+		}
+		
+		// update strikes stats display
 		$('#dataMessage').text(
 			droneStrikes.hitList.length + ' strikes since ' + 
 			droneStrikes.startTime.getMonth() + '/' +
