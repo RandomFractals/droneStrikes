8dc3bc3dbec03c02e3ea7fee6ecdbe03f04b7ece
Taras Novak <taras.novak@randomfractals.com>
2015-12-13 18:55:14 -0600

    added y grid lines + bars setup

--- prototype/scripts/hitGraph.js
+++ prototype/scripts/hitGraph.js
@@ -2,26 +2,17 @@
 * Hit graph view component.
 */
 function HitGraph(hitList, windowWidth) {
-	// init data table display
 	
+	var barWidth = 10;
 	var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = windowWidth - margin.left - margin.right - 120, // page margin
     height = 160 - margin.top - margin.bottom;
 
-	var x = d3.time.scale()
-    .range([0, width]);
-
-	var y = d3.scale.linear()
-    .range([height, 0]);
-
-	var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-	var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
+	var x = d3.time.scale().range([0, width]); //.rangeRound([0, width]);
+	var y = d3.scale.linear().range([height, 0]);
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+	var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+	
 	var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.minKills); });
@@ -50,8 +41,26 @@ function HitGraph(hitList, windowWidth) {
       .style("text-anchor", "end")
       .text("Kills");
 
+	svg.append("g")         
+      .attr("class", "grid")
+      .call(yAxis
+            .tickSize(-width, 0, 0)
+            .tickFormat("")
+        );
+	/*
+	svg.selectAll(".bar")
+    .data(hitList)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.date); })
+      .attr("width", '10') 
+      .attr("y", function(d) { return y(d.minKills); })
+      .attr("height", function(d) { return height - y(d.minKills); });
+	*/
+	
+	// line 
   svg.append("path")
       .datum(hitList)
       .attr("class", "line")
-      .attr("d", line);				
+      .attr("d", line);
 }
