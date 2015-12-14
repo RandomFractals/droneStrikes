71d22bc9383e993e9d46b2fd63785a52147416ee
Taras Novak <taras.novak@randomfractals.com>
2015-12-14 13:49:48 -0600

    rewrote list gen with jquery to make sense

--- prototype/scripts/hitList.js
+++ prototype/scripts/hitList.js
@@ -5,22 +5,41 @@ function HitList(listContainer, hitList) {
 	// init list data display
 	var listHtml = ''
 	var hit;
+	var hitListData = [];
 	for (var i = hitList.length-1; i >= 0; i--) {
-		hit = hitList[i];
-		listHtml += '<li class="list-group-item"><div class="list-group-item-heading>' +
-				'<button type="button" class="btn-link" href="#" onclick="zoomToHit(' + 
-					(hit.hitNumber-1) + 
-					')"><img src="images/map.png" width="32" height="32" /></button>' +
-				hit.dateString() + ' ' +
-				new HitBars(hit).toHtml(false) + // horizontal
-				'</div><p><a class="link" href="' + hit.link + '" target="_blank">' +
-					hit.narrative +
-				'</a></p>';
-		if ( hit.summary.length > 0 ) {
-			listHtml += '<p class="list-group-item-text">' + hit.summary + '</p>';
+		hit = hitList[i];		
+		
+		var li = $('<li/>').appendTo(listContainer);
+		var header = $('<div/>')
+			.addClass('list-group-item-heading')
+			.appendTo(li);
+			
+		var mapLink = $('<button/>')
+			.addClass('btn-link')
+			.attr('onclick', 'zoomToHit(' + (hit.hitNumber-1) + ')')
+			.appendTo(header);
+			
+		var mapImage = $('<img/>')
+			.attr('src', 'images/map.png')
+			.attr('width', 32)
+			.attr('height', 32)
+			.appendTo(mapLink);
+			
+		mapLink.after(	hit.dateString() );
+		
+		var storyLink = $('<a/>')
+			.addClass('link')
+			.attr('href', hit.link)
+			.attr('target', '_blank')
+			.text(hit.narrative)
+			.appendTo(header);
+			
+		if ( hit.summary.length > 0 ) {		
+			var story = $('<p/>')
+			.addClass('list-group-item-text')
+			.text(hit.summary)
+			.appendTo(li);
+			story.after('<br />');
 		}
-		listHtml +=	'<br /></li>';
 	}				
-	
-	listContainer.html(listHtml);	
 }
