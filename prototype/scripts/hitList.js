a8f623a066b729e1be38464298632aa6d189fd46
Taras Novak <taras.novak@randomfractals.com>
2015-12-14 12:43:52 -0600

    some bootstrap data list item fixes. geez!

--- prototype/scripts/hitList.js
+++ prototype/scripts/hitList.js
@@ -7,15 +7,15 @@ function HitList(listContainer, hitList) {
 	var hit;
 	for (var i = hitList.length-1; i >= 0; i--) {
 		hit = hitList[i];
-		listHtml += '<li class="list-group-item"><span>' +
-				'<a href="#" onclick="zoomToHit(' + 
-					(hit.hitNumber-1) + ')"><img src="images/map.png" width="32" height="32" /></a>' +
-				hit.dateString() + '' +
+		listHtml += '<li class="list-group-item"><div class="list-group-item-heading>' +
+				'<button type="button" class="btn-link" href="#" onclick="zoomToHit(' + 
+					(hit.hitNumber-1) + 
+					')"><img src="images/map.png" width="32" height="32" /></button>' +
+				hit.dateString() + ' ' +
 				new HitBars(hit).toHtml(false) + // horizontal
-			'</span><br />' + 
-				'<a href="' + hit.link + '" target="_blank">' +
+				'</div><p><a class="link" href="' + hit.link + '" target="_blank">' +
 					hit.narrative +
-				'</a>';
+				'</a></p>';
 		if ( hit.summary.length > 0 ) {
 			listHtml += '<p class="list-group-item-text">' + hit.summary;
 		}
